c7bb83752d6bb9487a7d8a338fb738423af67c5a
William Felker <gndclouds@icloud.com>
2021-01-14 21:03:30 +0800

    Initial commit

---
+++ scripts/sites.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Don't forget the comma! No trailing slashes
+// protocol://url.domain.ext
+
+const sites = [
+  { url: 'https://tinyfactories.space', title: 'tiny factories', type: 'tribe', author: 'gardener', contact: 'email', rss: 'blank', feed: 'blank' },
+  { url: 'http://www.streetdebater.com/', title: 'Street Debater', type: 'product', author: 'Tomo Kihara', contact: 'blank', rss: 'blank', feed: 'blank' },
+  { url: 'https://www.kickstarter.com/projects/1324099197/quickstarterbbcube-a-shouting-game-for-civilized-p?ref=nav_search&result=project&term=BBCubes', title: 'BBCubes', type: 'product', author: 'Weiwei Hsu', contact: 'blank', rss: 'blank', feed: 'blank' },
+  { url: 'https://www.tomokihara.com/en/code-lab.html', title: 'Is this Violence', type: 'product', author: 'Tomo Kihara', contact: 'blank', rss: 'blank', feed: 'blank' },
+  { url: 'https://blog.usejournal.com/behind-ccas-unofficial-mascot-da63d68f15c', title: 'CCA Max', type: 'Community', author: 'Weiwei Hsu', contact: 'blank', rss: 'blank', feed: 'blank' },
+  { url: 'https://scrtproj.org', title: 'Secret Project Taiwan', type: 'Community', author: 'blank', contact: 'Weiwei Hsu, Will Felker', rss: 'blank', feed: 'blank' },
+  { url: 'https://www.kickstarter.com/projects/weiweihsu/break-kickstarter-cyano-postcard?ref=user_menu', title: 'Cyano 🌊 Postcards', type: 'product', author: 'Weiwei Hsu', contact: 'blank', rss: 'blank', feed: 'blank' },
+  { url: 'https://https://weiweihsu.typeform.com/to/GwiV0H', title: 'Time Garden', type: 'product', author: 'blank', contact: 'blank', rss: 'blank', feed: 'blank' },
+  { url: 'https://tinytext.today', title: 'Tiny Text', type: 'service', author: 'blank', contact: 'blank', rss: 'blank', feed: 'blank' },
+  { url: 'https://ooak.space', title: 'arena2slides', type: 'product', author: 'blank', contact: 'blank', rss: 'blank', feed: 'blank' },
+  { url: 'http://indiepreneur.wiki', title: 'indiepreneur', type: 'wiki', author: 'gardener', contact: 'blank', rss: 'blank', feed: 'blank' }
+
+]
